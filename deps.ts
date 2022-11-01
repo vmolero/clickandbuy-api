826d7e194f8fb603a203126d3c081db7f1505bc8
Victor Molero <victor.molero@gmail.com>
2022-11-01 21:37:35 +0100

    add deps and docker files

--- deps.ts
+++ deps.ts
@@ -1,6 +1,12 @@
 // Deno STD
-export * as log from 'https://deno.land/std@0.160.0/log/mod.ts'
-export { config } from 'https://deno.land/std@0.160.0/dotenv/mod.ts'
+export * as log from 'https://deno.land/std@0.161.0/log/mod.ts'
+export { config } from 'https://deno.land/std@0.161.0/dotenv/mod.ts'
+export { crypto } from 'https://deno.land/std@0.161.0/crypto/mod.ts'
+export {
+  encode as base64encode,
+  decode as base64decode
+} from 'https://deno.land/std@0.161.0/encoding/base64url.ts'
+
 
 // AWS SIGN
 export { AWSSignerV4 } from 'https://deno.land/x/aws_sign_v4@1.0.2/mod.ts'
@@ -21,3 +27,7 @@ export type {
   OpineResponse,
   OpineRequest
 } from 'https://deno.land/x/opine@2.3.3/mod.ts'
+
+// Redis client
+export { connect } from 'https://deno.land/x/redis/mod.ts'
+export type { Redis } from 'https://deno.land/x/redis/mod.ts'
--- start-dev.sh
+++ start-dev.sh
@@ -1,1 +1,2 @@
+docker-compose -f ./docker/dev.yml up -d postgres redis
 deno run --allow-all --unstable --watch main.ts
